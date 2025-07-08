1235121fc167f9b01fdb66c9480aa26d32f37152
L-alam <lalam@bu.edu>
2025-07-08 15:40:07 -0400

    testing routes with movieid and onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text className="text-blue-500 text-4xl font-bold">Welcome!</Text>
+      <Text className="text-dark-100 text-4xl font-bold">Welcome!</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avangers">Avenger Movie</Link>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      primary: '#030014',
+      secondary: '#151312',
+      light: {
+        100: '#D6C6FF',
+        200: '#A8B5DB',
+        300: '#9CA4AB',
+      },
+      dark: {
+        100: '#221F3D',
+        200: '#0F0D23',
+      }
+    },
   },
   plugins: [],
 }
